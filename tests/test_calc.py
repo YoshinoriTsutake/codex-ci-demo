dc9dbd994095bc670b0962b230f497f6318ec7f8
YoshinoriTsutake <yoshi.tsutake@gmail.com>
2026-03-20 23:19:12 +0900

    Add edge case tests for add

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,3 +2,9 @@ from app.calc import add
 
 def test_add():
     assert add(2, 3) == 5
+
+def test_add_negative():
+    assert add(-2, -3) == -5
+
+def test_add_mixed_sign():
+    assert add(-2, 3) == 1
